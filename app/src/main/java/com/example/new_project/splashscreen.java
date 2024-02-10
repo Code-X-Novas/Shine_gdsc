12929470de22dc8890a0856a26fd004c2bdf3e80
Abhishek Bairagi <abhishekbairagi2005@gmail.com>
2024-02-10 09:13:02 +0530

    splashscreen backend

--- app/src/main/java/com/example/new_project/splashscreen.java
+++ app/src/main/java/com/example/new_project/splashscreen.java
@@ -1,5 +1,6 @@
 package com.example.new_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -13,10 +14,20 @@ public class splashscreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splashscreen);
-
-
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(splashscreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
 
     }
 }
