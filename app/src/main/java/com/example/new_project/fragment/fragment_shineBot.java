37a499894a46ca13f61ba7f781b7d2bea83777b5
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-02-18 12:22:34 +0530

    map section modified

--- app/src/main/java/com/example/new_project/fragment/fragment_shineBot.java
+++ app/src/main/java/com/example/new_project/fragment/fragment_shineBot.java
@@ -1,8 +1,11 @@
 package com.example.new_project.fragment;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,7 +14,36 @@ import android.view.ViewGroup;
 
 import com.example.new_project.R;
 
-public class fragment_shineBot extends Fragment {
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.QueryResult;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.SessionsSettings;
+import com.google.cloud.dialogflow.v2.TextInput;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.UUID;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class fragment_shineBot extends Fragment  {
+
+    private RecyclerView recyclerView;
+    private EditText editTextMessage;
+    private Button buttonSend;
+
+
     View layout;
     Activity activity;
     public fragment_shineBot(Activity activity){
@@ -22,12 +54,24 @@ public class fragment_shineBot extends Fragment {
 
     }
 
+
+
+
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         layout= inflater.inflate(R.layout.fragment_shine_bot, container, false);
+
+
+
+
+
+
+
         return layout;
     }
 
-    }
+
+   }
